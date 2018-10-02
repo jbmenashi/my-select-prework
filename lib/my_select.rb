9e82ad7fd67815289bdb3572b3b4d45cfdd62865
Jacob Menashi <jbmenashi@gmail.com>
2018-10-02 18:45:30 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,6 +3,13 @@ def my_select(collection)
    return false
  else
    i = 0 
+   final_array = []
    while i < collection.length
-    
+     yield(array[i])
+     if array[i] == true
+       final_array << array[i]
+     end
+   end
+   final_array
+ end
 end
